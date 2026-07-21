e71dca96e9b14d221284324064ef5662d1ba3cfc
tannermnelson <147342915+tannermnelson@users.noreply.github.com>
2026-07-21 18:11:15 -0400

    Added Marathon Prep Calendar
    Removed phase archives are the bottom as well

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "macro-hub-2026-07-20-v2";
+﻿const CACHE_NAME = "macro-hub-2026-07-21-v7";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -77,3 +77,10 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+
+
+
+
+
+
